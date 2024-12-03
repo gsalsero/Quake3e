adf1520ab4a0b74c9817fcee964ad847dc103953
gmoney <george.david@hp.com>
2024-12-03 11:38:49 -0700

    build with shell script

--- mingw-build.sh
+++ mingw-build.sh
@@ -3,11 +3,5 @@
 CFLAGS="${CFLAGS} -DISO_DATE=${ISO_DATE} -DBUILD_ID=${BUILD_ID}"
 export CFLAGS
 
-export MYFLAGS="${CFLAGS} -D_DELETE_ME=1"
-echo "MYFLAGS=${MYFLAGS}"
-
-echo '### env ####'
-env | sort
-cd "$SOURCES_DIR" || exit 1
 echo make -j "$(nproc)" install ARCH=x86_64 CC=gcc USE_SDL=0 USE_RENDERER_DLOPEN=0 RENDERER_DEFAULT=vulkan CNAME=quake3e-vulkan BUILD_SERVER=0
-
+make -j "$(nproc)" install ARCH=x86_64 CC=gcc USE_SDL=0 USE_RENDERER_DLOPEN=0 RENDERER_DEFAULT=vulkan CNAME=quake3e-vulkan BUILD_SERVER=0
