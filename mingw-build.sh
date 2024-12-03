606290487e0a437f4868a88a4ead946dbe991b12
gmoney <george.david@hp.com>
2024-12-03 11:29:40 -0700

    save some azure vars to env vars

--- mingw-build.sh
+++ mingw-build.sh
@@ -2,13 +2,13 @@
 export MYFLAGS="${CFLAGS} -D_DELETE_ME=1"
 echo "MYFLAGS=${MYFLAGS}"
 
-CFLAGS="-DISO_DATE=${ISO_DATE} -DBUILD_ID=$(Build.BuildId)"
+CFLAGS="-DISO_DATE=${ISO_DATE} -DBUILD_ID=${BUILD_ID}"
 export CFLAGS
 
 echo '### env ####'
 env | sort
-echo "Build.BuildId=${Build.BuildId}"
-echo "Build.SourcesDirectory=${Build.SourcesDirectory}"
-cd '$(Build.SourcesDirectory)' || exit 
+echo "Build.BuildId=${BUILD_ID}"
+echo "Build.SourcesDirectory=${SOURCES_DIR}"
+cd "$SOURCES_DIR" || exit 1
 echo make -j "$(nproc)" install ARCH=x86_64 CC=gcc USE_SDL=0 USE_RENDERER_DLOPEN=0 RENDERER_DEFAULT=vulkan CNAME=quake3e-vulkan BUILD_SERVER=0
 
