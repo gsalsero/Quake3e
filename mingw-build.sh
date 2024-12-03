1c8e95e54ac30a45a71984045dd9fcd173357084
gmoney <george.david@hp.com>
2024-12-03 11:46:46 -0700

    add execution tracing

--- mingw-build.sh
+++ mingw-build.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
+set -x
 
 CFLAGS="${CFLAGS} -DISO_DATE=${ISO_DATE} -DBUILD_ID=${BUILD_ID}"
 export CFLAGS
 
-echo make -j "$(nproc)" install ARCH=x86_64 CC=gcc USE_SDL=0 USE_RENDERER_DLOPEN=0 RENDERER_DEFAULT=vulkan CNAME=quake3e-vulkan BUILD_SERVER=0
 make -j "$(nproc)" install ARCH=x86_64 CC=gcc USE_SDL=0 USE_RENDERER_DLOPEN=0 RENDERER_DEFAULT=vulkan CNAME=quake3e-vulkan BUILD_SERVER=0
