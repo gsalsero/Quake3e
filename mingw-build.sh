203e6acc3a1b3324a6d70f1e2baa5ea1eb833e05
gmoney <george@miamisalsa.com>
2024-12-03 13:25:18 -0700

    set DESTDIR to bin and archive that dir

--- mingw-build.sh
+++ mingw-build.sh
@@ -4,6 +4,6 @@ set -x
 CFLAGS="${CFLAGS} -DISO_DATE=${ISO_DATE} -DBUILD_ID=${BUILD_ID}"
 export CFLAGS
 
-make -j "$(nproc)" install ARCH=x86_64 CC=gcc USE_SDL=0 USE_RENDERER_DLOPEN=0 RENDERER_DEFAULT=vulkan CNAME=quake3e-vulkan BUILD_SERVER=0
+make -j "$(nproc)" install DESTDIR=bin ARCH=x86_64 CC=gcc USE_SDL=0 USE_RENDERER_DLOPEN=0 RENDERER_DEFAULT=vulkan CNAME=quake3e-vulkan BUILD_SERVER=0
 make clean ARCH=x86_64
-make -j "$(nproc)" install ARCH=x86_64 CC=gcc USE_SDL=0 USE_RENDERER_DLOPEN=0 RENDERER_DEFAULT=opengl
+make -j "$(nproc)" install DESTDIR=bin ARCH=x86_64 CC=gcc USE_SDL=0 USE_RENDERER_DLOPEN=0 RENDERER_DEFAULT=opengl
