4f5fa3bb6df3d1b0b83e5c2673bc290c00d6cd56
gmoney <george.david@hp.com>
2024-12-03 12:31:49 -0700

    clean between different builds

--- mingw-build.sh
+++ mingw-build.sh
@@ -5,4 +5,5 @@ CFLAGS="${CFLAGS} -DISO_DATE=${ISO_DATE} -DBUILD_ID=${BUILD_ID}"
 export CFLAGS
 
 make -j "$(nproc)" install ARCH=x86_64 CC=gcc USE_SDL=0 USE_RENDERER_DLOPEN=0 RENDERER_DEFAULT=vulkan CNAME=quake3e-vulkan BUILD_SERVER=0
+make clean ARCH=x86_64
 make -j "$(nproc)" install ARCH=x86_64 CC=gcc USE_SDL=0 USE_RENDERER_DLOPEN=0 RENDERER_DEFAULT=opengl
